4893f07357fe091fa6973128965cba7eb09decde
Gustavo Oliveira <gof@cin.ufpe.br>
2019-01-07 01:34:42 -0300

    mudando config

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # catalogodiscos-backend
+
+### documentações úteis
+
+https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_ConnectToPostgreSQLInstance.html
+http://postgresguide.com/utilities/psql.html
--- server/migrations/20181202171723-create-collection.js
+++ server/migrations/20181202171723-create-collection.js
@@ -27,18 +27,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    }).then(function () {
-      Collection.create({
-        name: 'Internacional',
-        description: 'Discos internacionais, que marcaram gerações.',
-        url: 'https://images.vexels.com/media/users/3/145816/isolated/preview/7616b64374d1ecc318e9d638807c4d61-logotipo-de-sinal-de-msica-rock-by-vexels.png'
-      });
-
-      Collection.create({
-        name: 'Nacionais',
-        description: 'Discos nacionais, que marcaram gerações.',
-        url: 'https://st.depositphotos.com/1609126/3723/i/450/depositphotos_37238347-stock-photo-brazil-flag.jpg'
-      });
     })
   },
   down: (queryInterface, Sequelize) => {
